53a510f5d90e9f1ef4948e0e249bcc9020555851
Korny666 <korny@publicgrave.de>
2024-08-14 09:35:05 +0200

    setting eslint_d as default

--- configs/ale.nix
+++ configs/ale.nix
@@ -1,20 +1,23 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   configs.ale = {
     plugins = [ pkgs.vimPlugins.ale ];
      lua = [
+      "vim.g.ale_javascript_eslint_executable = 'eslint_d'"
+      "vim.g.ale_javascript_eslint_use_global = 1"
       "vim.g.ale_linter_aliases = { vue = {'javascript'} }"
-      "vim.g.ale_linters = { 
+      "vim.g.ale_linters = {
          javascript = {'eslint'},
          typescript = {'eslint'},
          vue = {'eslint'}
       }"
-      "vim.g.ale_fixers = { 
+      "vim.g.ale_fixers = {
          javascript = {'eslint'},
          typescript = {'eslint'},
          vue = {'eslint'}
       }"
       "vim.g.ale_fix_on_save = 1"
     ];
+    env.PATH.values = [ "${lib.makeBinPath [ pkgs.eslint_d ]}" ];
   };
 }
